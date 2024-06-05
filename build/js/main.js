62ee01ea668f48d35c08073914a4917a5f18590b
tylerdurden2k2 <phamquanghuy2809@gmail.com>
2024-06-05 16:08:29 +0700

    animation button

---
+++ build/js/main.js
@@ -0,0 +1,16 @@
+const initApp = () => {
+	const menuBtn = document.getElementById("mobile-open-button");
+
+	const mobileMenu = document.getElementById("mobile-menu");
+
+	const toggleMenu = () => {
+		mobileMenu.classList.toggle("hidden");
+		mobileMenu.classList.toggle("flex");
+		menuBtn.classList.toggle("toggle-btn");
+	};
+
+	menuBtn.addEventListener("click", toggleMenu);
+	mobileMenu.addEventListener("click", toggleMenu);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./build/*.html"],
+	content: ["./build/*.html", "./build/js/*.js"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				widescreen: { raw: "(min-aspect-ration: 3/2)" },
+				tallscreen: { raw: "(min-aspect-ration: 13/20)" },
+			},
+
+			keyframes: {
+				"open-menu": {
+					"0%": { transform: "scaleY(0)" },
+					"60%": { transform: "scaleY(1.2)" },
+					"100%": { transform: "scaleY(1)" },
+				},
+			},
+
+			animation: {
+				"open-menu": "open-menu 500ms ease-in-out forwards",
+			},
+		},
 	},
 	plugins: [],
 };
